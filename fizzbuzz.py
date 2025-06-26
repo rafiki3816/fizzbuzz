465e79b6ffe27381e937d62752469aacb3ab94da
junegood <junegood@naver.com>
2025-06-26 17:51:32 +0900

    feat: Print buzz if i is divisible by 5.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,7 @@
-for i in range(1, 16+1):
-	if i%3 == 0:
+for j in range(1, 16+1):
+	if j%3 == 0:
 	  print('fizz')
+        elif j%5 == 0:
+          print('buzz')
         else:
           print(i)
